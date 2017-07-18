975f7e107baa7097231496e3b63b16717bd94365
Lisa Huang <lisa.yc.huang@gmail.com>
2017-07-18 18:05:50 +0300

    Refactored EDIT course code. Created GET "/courses/:id/edit" route with corresponding view, "/courses/edit.erb" for instructors to edit course.

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,5 +1,6 @@
 class CourseController < ApplicationController
     
+    # View all courses
     get '/courses' do
         @courses = Course.all
         @current_user = current_user
@@ -7,7 +8,7 @@ class CourseController < ApplicationController
         erb :"/courses/index"
     end
 
-    # only instructors can create new courses
+    # only instructors can view form to create a new course
     get '/courses/new' do
         if current_user.instructor 
             erb :"/courses/new"
@@ -16,14 +17,16 @@ class CourseController < ApplicationController
         end
     end
 
+    # Instructor CREATE a new course
     post '/courses' do
         @new_course = Course.create(name: params[:name], icon: params[:icon], description: params[:descrciption], level: params[:level].to_i-1, instructor_id: session[:user_id])
         @new_course.save
         redirect to "/courses"
     end
 
+    # View a single course page
     get '/courses/:id' do
-        @course = Course.find_by_id(params[:id])
+        @course = find_course(params[:id])
         @existing_registration = nil
         if !current_user.instructor
             @existing_registration = UserCourse.where("course_id = ? AND user_id = ?", params[:id], current_user.id).first
@@ -36,8 +39,13 @@ class CourseController < ApplicationController
         end
     end
 
+    get '/courses/:id/edit' do
+        @course = find_course(params[:id])
+        erb :"/courses/edit"
+    end
+
     patch '/courses/:id' do
-        @course = Course.find_by_id(params[:id])
+        @course = find_course(params[:id])
         @course.update(name: params[:name], icon: params[:icon], description: params[:description], level: params[:level].to_i-1)
 
         if current_user.instructor && current_user.id === @course.instructor.id
@@ -46,7 +54,7 @@ class CourseController < ApplicationController
                 redirect to "/courses/#{@course.id}"
             else 
                 flash[:message] = "Something went wrong. Please try to edit course again."
-                redirect to "/courses/#{@course.id}"
+                redirect to "/courses/#{@course.id}/edit"
             end
         else
             redirect to "/courses/#{@course.id}"
