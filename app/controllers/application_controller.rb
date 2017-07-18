a8b80b4e938ed53dbd213ba1f7aa382fdbb12086
Lisa Huang <lisa.yc.huang@gmail.com>
2017-07-18 18:55:30 +0300

    Added notes to ApplicationController for ROUTE and HELPER METHODS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,39 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   enable :sessions
+  use Rack::Flash
 
   set :session_secret, "secret_courses"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  use Rack::Flash
-  
+  # User will only see homepage IF they are not currently logged in
   get '/' do
-    erb :index
+    if logged_in?
+        redirect to "/courses"
+    else
+        erb :index
+    end
   end
 
+  # HELPER METHODS
   helpers do
+
+    # Check if a user is logged into their account
     def logged_in?
       !!session[:user_id]
     end
     
+    # Find current user
     def current_user
       User.find_by_id(session[:user_id])
     end
 
+    # Find a course based on id
+    def find_course(id)
+      Course.find_by_id(id)
+    end
+
+    # Display course difficulty based on level
     def course_difficulty(num)
         if num === 0
           "Easy"
@@ -32,6 +46,7 @@ class ApplicationController < Sinatra::Base
         end
     end
 
+    # Display course registration status based on confirmation
     def registration_status(num)
         if num === 0
           "Pending"
@@ -40,10 +55,6 @@ class ApplicationController < Sinatra::Base
         else
           "Enrolled"
         end
-    end
-    
-    def find_course(id)
-      Course.find_by_id(id)
     end
   end
 
