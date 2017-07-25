c97b55e008bb9808b1fc6aea442c74c839896ec7
Lisa Huang <lisa.yc.huang@gmail.com>
2017-07-25 18:26:13 +0000

    Updated User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-
-  has_many :courses_teaching, class_name: "Course", primary_key: "instructor_id"
-  has_many :courses_studying, class_name: "Course", through: :user_courses
-
   validates :full_name, :username, :email, presence: true
-
-
-  def slug
-        self.username.gsub(/\s/, '-').downcase
-  end
-
-  def self.find_by_slug(slugged)
-      self.all.find {|user| user.slug == slugged}
-  end
   
 end
